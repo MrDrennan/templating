cdd16c33071bc4b1fe29cef299e7ee10d0104a74
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-01-27 13:40:29 -0500

    added title variable

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ require("vendor/autoload.php");
 // :: means to invoke a static method. -> means to use an instance method
 $f3 = Base::instance();
 
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+    $f3->set('title', 'Templating');
     $view = new Template();
     echo $view->render('views/home.html');
-    //echo "Hello Food";
 });
 
 $f3->run();
